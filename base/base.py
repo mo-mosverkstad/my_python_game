3fa6e8d60bf7013a430399f1ee626cc48b215a71
mo-mosverkstad <mo.mosverkstad@gmail.com>
2018-02-10 13:14:33 +0100

    Base for pygame is added

---
+++ base/base.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import pygame, sys
+from pygame.locals import *
+
+# CONSTANTS DEFINITION
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+SCREEN_TITLE = 'MOS PYGAME CODING'
+
+# VARIABLES DEFINITION
+continueFlag = True
+
+
+# PYGAME INITIALISATION
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption(SCREEN_TITLE)
+
+
+# PYGAME DEAD LOOP FOR RECEIVING EVENTS
+while continueFlag: # main game loop
+
+    # EVENTS HANDLING
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continueFlag = False
+            print('pygame exit!')
+            pygame.display.quit()
+            pygame.quit()
+
+    # PAINT THE SCREEN
